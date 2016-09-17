6cee84db59aa405ab19aeb87ab184ca960aba26b
Usuário Convidado <Guest@L2602MICRO13.local>
2016-09-17 13:42:39 -0300

    API de cep e views

---
+++ MobileDev/MobileDev/Models/CepResultModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace MobileDev
+{
+	public class CepResultModel
+	{
+		public CepResultModel()
+		{
+		}
+	}
+}
+
---
+++ MobileDev/MobileDev/Pages/EnderecoPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace MobileDev
+{
+	public partial class EnderecoPage : ContentPage
+	{
+		public EnderecoPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
+
